ad1ff71de0b66bfb98bee3e69503340a9a0a6028
Rolf Smit <rolf@neotechsoftware.com>
2019-04-15 17:19:10 +0200

    Update dependencies + migrate to AndroidX

--- plugin/src/test/test-fixtures/multi-module/app/src/main/java/org/neotech/app/MainActivity.kt
+++ plugin/src/test/test-fixtures/multi-module/app/src/main/java/org/neotech/app/MainActivity.kt
@@ -1,7 +1,7 @@
 package org.neotech.app
 
 import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import org.neotech.library.android.LibraryAndroidKotlin
--- plugin/src/test/test-fixtures/multi-module/library_android/src/androidTest/java/org/neotech/library/android/LibraryAInstrumentedTest.java
+++ plugin/src/test/test-fixtures/multi-module/library_android/src/androidTest/java/org/neotech/library/android/LibraryAInstrumentedTest.java
@@ -1,6 +1,6 @@
 package org.neotech.library.android;
 
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
