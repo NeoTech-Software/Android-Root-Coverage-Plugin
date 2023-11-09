c468798bb892343cdea81a4c5ed5ae953b01ed53
Rolf Smit <rolf@neotech.nl>
2023-11-09 20:30:42 +0100

    Add: support for configuration cache
    Fixes: #81

--- plugin/src/main/kotlin/org/neotech/plugin/rootcoverage/CustomJacocoReportTask.kt
+++ plugin/src/main/kotlin/org/neotech/plugin/rootcoverage/CustomJacocoReportTask.kt
@@ -2,11 +2,13 @@ package org.neotech.plugin.rootcoverage
 
 import org.gradle.api.file.ConfigurableFileTree
 import org.gradle.api.file.Directory
+import org.gradle.api.file.FileTree
 import org.gradle.api.file.RegularFile
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.util.PatternSet
 import org.gradle.testing.jacoco.tasks.JacocoReport
 import org.neotech.plugin.rootcoverage.utilities.fileTree
 
@@ -21,9 +23,11 @@ abstract class CustomJacocoReportTask : JacocoReport() {
     fun allDirectories(): ListProperty<Directory> {
         val files = project.objects.listProperty(Directory::class.java)
 
-        val filteredFiles: Provider<List<ConfigurableFileTree>> = files.map {
+        val filteredFiles: Provider<List<FileTree>> = files.map {
+            val patternSet = PatternSet()
+            patternSet.exclude(excludePatterns.get())
             it.map { directory ->
-                project.fileTree(directory.asFile, excludes = excludePatterns.get())
+                directory.asFileTree.matching(patternSet)
             }
         }
         classDirectories.from(filteredFiles)
